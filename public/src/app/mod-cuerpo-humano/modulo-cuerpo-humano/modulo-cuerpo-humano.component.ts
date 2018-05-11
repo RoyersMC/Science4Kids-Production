a4d7429fca724c5b004b54c984e2837e6c6bbb1b
DenisseGS <denisseags@hotmail.com>
2018-05-10 23:39:45 -0600

    cambios en los modulos

---
+++ public/src/app/mod-cuerpo-humano/modulo-cuerpo-humano/modulo-cuerpo-humano.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modulo-cuerpo-humano',
+  templateUrl: './modulo-cuerpo-humano.component.html',
+})
+export class ModuloCuerpoHumanoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ public/src/app/mod-oceano/modulo-info-oceano/modulo-info-oceano.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modulo-info-oceano',
+  templateUrl: './modulo-info-oceano.component.html',
+})
+export class ModuloInfoOceanoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
