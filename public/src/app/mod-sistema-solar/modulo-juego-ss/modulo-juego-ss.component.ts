3229ed8d3eea473528cfc9c5a5eab02bfdd96f4f
RoyersMC <royers.m.c93@hotmail.com>
2018-06-05 10:57:43 -0600

    Juego optimizado

--- public/src/app/mod-sistema-solar/modulo-juego-ss/modulo-juego-ss.component.ts
+++ public/src/app/mod-sistema-solar/modulo-juego-ss/modulo-juego-ss.component.ts
@@ -3,33 +3,62 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-modulo-juego-ss',
   templateUrl: './modulo-juego-ss.component.html',
-  
-  
+
+
 })
 export class ModuloJuegoSsComponent implements OnInit {
 
-  public direccion1 = "/assets/images/iconos/mercurioIcon.png";
-  public direccion2 = "/assets/images/iconos/venusIcon.png";
-  public direccion3 = "/assets/images/iconos/tierraIcon.png";
-  public direccion4 = "/assets/images/iconos/marteIcon.png";
-  public direccion5 = "/assets/images/iconos/jupiterIcon.png";
-  public direccion6 = "/assets/images/iconos/saturnoIcon.png";
-  public direccion7 = "/assets/images/iconos/uranoIcon.png";
-  public direccion8 = "/assets/images/iconos/neptunoIcon.png";
+  receivedDataMercurio: Array<any> = [];
+  receivedDataVenus: Array<any> = [];
+  receivedDataTierra: Array<any> = [];
+  receivedDataMarte: Array<any> = [];
+  receivedDataJupiter: Array<any> = [];
+  receivedDataSaturno: Array<any> = [];
+  receivedDataUrano: Array<any> = [];
+  receivedDataNeptuno: Array<any> = [];
 
-  //public direccion = this.direccion3;
-  
-  //transferData: Object = {msg: '/assets/images/iconos/solIcon.png'};
-  //transferdata: string="/assets/images/iconos/solIcon.png";
-  receivedData: Array<any> = [];
+  transferDataSuccessMercurio($event: any) {
+    this.receivedDataMercurio.push($event);
 
-  transferDataSuccess($event: any) {
-      this.receivedData.push(this.direccion8);
+  }
+  transferDataSuccessVenus($event: any) {
+
+    this.receivedDataVenus.push($event);
+
+  }
+  transferDataSuccessTierra($event: any) {
+
+    this.receivedDataTierra.push($event);
+
+  }
+  transferDataSuccessMarte($event: any) {
+
+    this.receivedDataMarte.push($event);
+
+  }
+  transferDataSuccessJurpiter($event: any) {
+
+    this.receivedDataJupiter.push($event);
+
+  }
+  transferDataSuccessSaturno($event: any) {
+
+    this.receivedDataSaturno.push($event);
+
+  }
+  transferDataSuccessUrano($event: any) {
+
+    this.receivedDataUrano.push($event);
+
+  }
+  transferDataSuccessNeptuno($event: any) {
+
+    this.receivedDataNeptuno.push($event);
   }
 
-  
+
   constructor() {
-   }
+  }
 
   ngOnInit() {
   }
