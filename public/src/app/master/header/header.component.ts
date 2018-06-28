9fc755f3750e071a243a33baf266dc3c15c7900b
DenisseGS <denisseags@hotmail.com>
2018-06-28 14:14:27 -0600

    Commit

--- public/src/app/master/header/header.component.ts
+++ public/src/app/master/header/header.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
+
+  video(){
+    $("#spotFondo").show();
+    var v = document.getElementsByTagName("video")[0];
+    v.play();
+  }  
+
+  cerrarVideo(){
+    document.getElementById('spotFondo').style.display = 'none';
+    var v = document.getElementsByTagName("video")[0];
+    v.pause();
+    v.currentTime = 0;
+  }
 
   constructor() { }
 
