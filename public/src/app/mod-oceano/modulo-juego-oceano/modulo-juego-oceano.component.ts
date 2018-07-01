c6dc34a698039ce445f84bc0e7acb90061183266
hjgx <huanxo10@gmail.com>
2018-06-30 21:44:43 -0600

    .

--- public/src/app/mod-oceano/modulo-juego-oceano/modulo-juego-oceano.component.ts
+++ public/src/app/mod-oceano/modulo-juego-oceano/modulo-juego-oceano.component.ts
@@ -6,46 +6,55 @@ declare var slick: number;
 })
 export class ModuloJuegoOceanoComponent implements OnInit {
 contador: number = 0;
+imagevar: any;
 
  ocultartortuga() {
   this.contador = this.contador + 1;
-   document.getElementById('img1').style.display = 'none';
+  document.getElementById('img1').style.display = 'none';
+  document.getElementById('img11').style.display = 'block';
    this.ganar();
  }
 
  ocultarpulpo() {
   this.contador++;
   document.getElementById('img2').style.display = 'none';
+  document.getElementById('img22').style.display = 'block';
   this.ganar();
 }
 ocultaranguila() {
   this.contador++;
   document.getElementById('img3').style.display = 'none';
+  document.getElementById('img33').style.display = 'block';
   this.ganar();
 }
 ocultarcangrejo() {
   this.contador++;
   document.getElementById('img4').style.display = 'none';
+  document.getElementById('img44').style.display = 'block';
   this.ganar();
 }
 ocultarpezangel() {
   this.contador++;
   document.getElementById('img5').style.display = 'none';
+  document.getElementById('img55').style.display = 'block';
   this.ganar();
 }
 ocultarestrella() {
   this.contador++;
   document.getElementById('img6').style.display = 'none';
+  document.getElementById('img66').style.display = 'block';
   this.ganar();
 }
 ocultarcangre() {
   this.contador++;
   document.getElementById('img7').style.display = 'none';
+  document.getElementById('img77').style.display = 'block';
   this.ganar();
 }
 ocultarlabrido() {
   this.contador++;
   document.getElementById('img8').style.display = 'none';
+  document.getElementById('img88').style.display = 'block';
   this.ganar();
 }
 
